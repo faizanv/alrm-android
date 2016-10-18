3eb6f3cd1741d3c54d5aeb2faff86c53e7303f52
faizanv <faizan.virani.44@gmail.com>
2016-10-18 15:57:50 -0400

    Dummy card with correct looking UI made. Added firebase json file.

--- app/src/main/java/mas/alrm/alrm/MainActivity.java
+++ app/src/main/java/mas/alrm/alrm/MainActivity.java
@@ -1,13 +1,12 @@
 package mas.alrm.alrm;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +17,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public void signin(View view) {
+        Intent intent = new Intent(this, UpcomingAlarmsActivity.class);
+        startActivity(intent);
     }
 
     @Override
